8551277b7394fc7de10a94dcc0a63182c0483e88
alex0207s <alexliao-dev@proton.me>
2025-12-19 03:44:59 +0800

    remove unused env variable

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,13 @@
 import type { NextConfig } from "next";
 
-if (!process.env.CI) {
-  require("./src/lib/env");
-}
-
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
